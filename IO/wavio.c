afe76539aba94371f2fcf97e2dd77b6ec81c9296
Chenchen Guo <gcc9108@gmail.com>
2016-08-04 01:46:32 -0700

    Fixed the pwlog2() function. Finished implementing the Integer arithmatic version.

--- IO/wavio.c
+++ IO/wavio.c
@@ -135,6 +135,125 @@ inline unsigned char flin2mu(int16_t input_frame)
 	return return_value;
 }
 
+void flin2mu_encode(int16_t* input_samples, uint8_t* output_samples, uint32_t sample_number)
+{
+	register int i;
+	register int16_t* input_sample_pointer = input_samples;
+	register uint8_t* output_sample_pointer = output_samples;
+	register int16_t sample;
+	register uint8_t sign_bit;
+	register uint16_t magnitude;
+	register double x, result;
+
+	for(i = sample_number; i > 0; i--){
+		sample = *input_sample_pointer;
+		sign_bit = ((uint16_t)sample) >> 15;
+		magnitude = (sign_bit)? -sample: sample;
+		if(magnitude > 32767)
+			magnitude = 32767;
+		x = magnitude / 32767.0;
+		result = (log(1.0 + MU * x)/log(1.0 + MU));
+		*output_sample_pointer = ~(((uint8_t)rint(result * 127.0)) | (sign_bit << 7));
+		input_sample_pointer++;
+		output_sample_pointer++;
+	}
+}
+
+uint16_t pwlog2(uint16_t X)
+{
+	if(X < (1 << 8))
+		return (-1);
+	if(X < (1 << 9))
+		return (X- (1 << 8));
+	if(X < (1 << 10))
+		return (X >> 9);
+	if(X < (1 << 11))
+		return ((X >> 10) + (1 << 8));
+	if(X < (1 << 12))
+		return ((X >> 11) + (1 << 9));
+	if(X < (1 << 13))
+		return ((X >> 12) + 768);
+	if(X < (1 << 14))
+		return ((X >> 13) + (1 << 10));
+	if(X < (1 << 15))
+		return ((X >> 14) + 1280);
+	if(X < (1 << 16))
+		return ((X >> 15) + 1536);
+	return (-1); // It should never reach this point
+}
+
+float fpwlog2(float x)
+{
+	if(x < 1.0){
+		return -1.0;
+	}
+	if(x < 2.0){
+		return x - 1.0;
+	}
+	if(x < 4.0){
+		return 1.0 + (x - 2.0) / 2.0;
+	}
+	if(x < 8.0){
+		return 2.0 + (x - 4.0) / 4.0;
+	}
+	if(x < 16.0){
+		return 3.0 + (x - 8.0) / 8.0;
+	}
+	if(x < 32.0){
+		return 4.0 + (x - 16.0)/ 16.0;
+	}
+	if(x < 64.0){
+		return 5.0 + (x - 32.0)/ 32.0;
+	}
+	if(x < 128.0){
+		return 6.0 + (x - 64.0)/ 64.0;
+	}
+	if(x < 256.0){
+		return 7.0 + (x - 128.0)/ 128.0;
+	}
+	if(x < 512.0){
+		return 8.0 + (x - 256.0)/ 256.0;
+	}
+	if(x < 1024.0){
+		return 9.0 + (x - 512.0)/ 512.0;
+	}
+	if(x < 2048.0){
+		return 10.0 + (x - 1024.0)/ 1024.0;
+	}
+	if(x < 4096.0){
+		return 11.0 + (x - 2048.0)/ 2048.0;
+	}
+	return -1;
+}
+
+void lin2mu_encode(int16_t* input_samples, uint8_t* output_samples, uint32_t sample_number)
+{
+	register uint32_t i;
+	register int16_t* input_sample_pointer = input_samples;
+	register uint8_t* output_sample_pointer = output_samples;
+	register int16_t sample;
+	register uint32_t sign_bit, magnitude, x, result;
+
+	for(i = sample_number; i > 0; i--){
+		sample = *input_sample_pointer;
+		sign_bit = (((uint16_t)sample) >> 15) << 7;
+		magnitude = (sign_bit)? -sample: sample;
+		if(magnitude > 32767)
+			magnitude = 32767;
+		// x = 1 + mu * magnitude; mu == 255
+		x = (((magnitude << 8) - magnitude) >> 15) + 1; // No rounding
+		// result = (fpwlog2(x) * 15.875 );// << 4; // result = 1/8 * log_2(1 + mu * x) * 127.0
+		result = (pwlog2(x << 8) >> 4);		
+		// if(result > 127){
+		// 	printf("Overflow detected\n");
+		// 	result = 127;
+		// }
+		*output_sample_pointer = ~(result | sign_bit);
+		input_sample_pointer++;
+		output_sample_pointer++;
+	}
+}
+
 const char *byte_to_binary(int x)
 {
 	static char b[9];
@@ -180,7 +299,9 @@ int main(int argc, char const *argv[])
 		printf("usage: %s <filename>\n", argv[0]);
 		exit(1);
 	}
+	printf("Loading WAV file: %s...\n", argv[1]);
 	wavread(argv[1], &samples, &data_chunk_size);
+	printf("File loaded into memory.\n");
 	printf("No. of channels:   %d\n",      header->n_channel);
 	printf("RIFF chunk size:   %d\n",      header->riff_chunk_size);
 	printf("FMT chunk size:    %d\n",      header->fmt_chunk_size);
@@ -231,22 +352,23 @@ int main(int argc, char const *argv[])
 		int16_t sample;
 		register int16_t* sample_pointer = (int16_t*) samples;
 		printf("Sample number: %d\n", sample_number);
-		uint8_t output_samples[sample_number];
-		register uint8_t* output_sample_pointer = output_samples;
+		// uint8_t output_samples[sample_number];
+		register uint8_t* output_sample_pointer = samples;
 		gettimeofday(&tvBegin, NULL);
 		printf("Compression started at: \n");
 		timeval_print(&tvBegin);
-		for(i = sample_number; i > 0; i--){
-			sample = *sample_pointer;
+		// for(i = sample_number; i > 0; i--){
+		// 	sample = *sample_pointer;
 
-			// if(sample_number - i < 50){
-			// 	printf("Sample [%d] = %d\n", sample_number - i, sample);
-			// }
-			// *output_sample_pointer = ~flin2mu(sample, MU, sample_number - i < 50);
-			*output_sample_pointer = ~flin2mu(sample);
-			sample_pointer++;
-			output_sample_pointer++;
-		}
+		// 	// if(sample_number - i < 50){
+		// 	// 	printf("Sample [%d] = %d\n", sample_number - i, sample);
+		// 	// }
+		// 	// *output_sample_pointer = ~flin2mu(sample, MU, sample_number - i < 50);
+		// 	*output_sample_pointer = ~flin2mu(sample);
+		// 	sample_pointer++;
+		// 	output_sample_pointer++;
+		// }
+		lin2mu_encode((int16_t*)samples, samples, sample_number);
 		gettimeofday(&tvEnd, NULL);
 		printf("Compression finished at: \n");
 		timeval_print(&tvEnd);
@@ -254,7 +376,7 @@ int main(int argc, char const *argv[])
 		timeval_subtract(&tvDiff, &tvEnd, &tvBegin);
 		printf("%ld.%06ld\n", tvDiff.tv_sec, tvDiff.tv_usec);
 		create_mu_header(sample_number);
-		wavwrite(argc == 3? argv[2]: "output.wav", output_samples, sample_number);
+		wavwrite(argc == 3? argv[2]: "output.wav", samples, sample_number);
 	}else if(header->bits_per_sample == 32){
 		errx(1, "The current version can not handle 32 bit floating point WAV file");
 	}
